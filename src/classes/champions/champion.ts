01bfd0e3ead35fce6841faefaa31cbc46d5ad82d
Matthew Kaufer <mjkaufer@gmail.com>
2023-11-26 15:10:20 -0500

    use static for class

--- src/classes/champions/champion.ts
+++ src/classes/champions/champion.ts
@@ -1,13 +1,20 @@
 import { LATEST_PATCH, PATCH_VERSIONS, TPatchVersion } from "@/data/constants/patchVersions";
 
 export class Champion {
-  protected startVersion: TPatchVersion;
-  protected endVersion: TPatchVersion;
+  // These should be set on champion def if a champ gets reworked, so we can
+  // validate if a given champ invoked is being run on a valid version / filter
+  // champs out of their versions
+  static readonly startVersion: TPatchVersion = PATCH_VERSIONS[0];
+  static readonly endVersion: TPatchVersion = LATEST_PATCH;
+  
   protected championName: string;
+  protected version: TPatchVersion;
 
-  constructor(championName: string) {
+  constructor(championName: string, version: TPatchVersion) {
     this.championName = championName;
-    this.startVersion = PATCH_VERSIONS[0];
-    this.endVersion = LATEST_PATCH;
+    this.version = version;
+    // TODO: Validate version is between start/end when we get semver code
   }
+
+
 }
