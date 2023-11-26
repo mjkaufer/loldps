ded1741194e15c0127e51f242cb0a32a2028a2b5
Matthew Kaufer <mjkaufer@gmail.com>
2023-11-26 15:06:50 -0500

    protected

--- src/classes/champions/champion.ts
+++ src/classes/champions/champion.ts
@@ -1,15 +1,13 @@
 import { LATEST_PATCH, PATCH_VERSIONS, TPatchVersion } from "@/data/constants/patchVersions";
 
 export class Champion {
-  private startVersion: TPatchVersion;
-  private endVersion: TPatchVersion;
-  private championName: string;
+  protected startVersion: TPatchVersion;
+  protected endVersion: TPatchVersion;
+  protected championName: string;
 
   constructor(championName: string) {
     this.championName = championName;
     this.startVersion = PATCH_VERSIONS[0];
     this.endVersion = LATEST_PATCH;
   }
-
-  
 }
